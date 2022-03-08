d1634a3f0a1fd355de4d7de4abc470962fa51879
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-03-09 02:41:24 +0600

    service api creation

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const { MongoClient } = require("mongodb");
 const cors = require('cors');
-const objectId = require('mongodb').ObjectId;
 require('dotenv').config();
+
 
 //Middle were ------------------------------------------
 app.use(cors());
@@ -12,13 +12,63 @@ app.use(express.json());
 //Port---------------------------------------------------
 const port = process.env.PORT || 5000;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ne473.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(idToken);
+      req.decodedUserEmail = decodedUser.email;
+    }
+    catch {
+
+    }
+  }
+  next();
+}
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    const database = client.db("personalService");
+    const serviceCollection = database.collection("service");
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.ne473.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+    
+
+   
+
+    // single food  ---------------------------------
+    app.post('/service', async (req, res) => {
+      const serviceData = req.body;
+      const result = await serviceCollection.insertOne(serviceData)
+      res.send(result)
+      console.log(result);
+    })
+
+    
+      
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+//test get -----------------------------------------------
+app.get('/', (req, res) => {
+  res.send('connected');
+})
+// listening port -----------------------------------------
+app.listen(port, () => {
+  console.log('listening port', port);
 });
